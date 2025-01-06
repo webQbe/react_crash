e93af1546920adcde66f6523a17c34a8d3a28d42
webQbe <webcubesolutionslk@gmail.com>
2025-01-06 11:09:47 +0530

    Modified index.html & src/main.jsx files

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Passing #root to createRoot()
+// Render whatever you want with render()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1>Hello</h1>
   </StrictMode>,
 )
