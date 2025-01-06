df4ad114d4aac399a091d170df39a390795f6474
webQbe <webcubesolutionslk@gmail.com>
2025-01-06 12:07:24 +0530

    Adding multiple elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <>
+      <div className='text-5xl'>App</div>
+      <p>Hello</p>
+    </>
+    /* <p>Hello</p> Error: Must have one parent element 
+      You can wrap multiple elements inside a parent div
+      If you don't want an actual parent div, you can use empty html tag <> </> 
+    */
   )
 }
 
