3f33b4f2b78883fabfdfb624793bbf9700651963
webQbe <webcubesolutionslk@gmail.com>
2025-01-07 10:40:57 +0530

    Created Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 /* rafce */
 import Navbar from './components/Navbar'; /* Import component */
+import Hero from './components/Hero';
 import React from 'react'
 
 const App = () => {
   return (
     <>
-      {/* Using Navbar Component 
+      {/* Using Components 
          Use Devloper Tools in the browser to see components tab */}
       <Navbar />
+      <Hero />
 
-      {/* <!-- Hero --> */}
-      <section className="bg-indigo-700 py-20 mb-4">
-        <div
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-        >
-          <div className="text-center">
-            <h1
-              className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-            >
-              Become a React Dev
-            </h1>
-            <p className="my-4 text-xl text-white">
-              Find the React job that fits your skills and needs
-            </p>
-          </div>
-        </div>
-      </section>
 
       {/* <!-- Developers and Employers --> */} 
       <section className="py-4">
