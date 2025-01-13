2dd888ac0f5d801bef51b3f01e83832998de854c
webQbe <webcubesolutionslk@gmail.com>
2025-01-13 13:37:16 +0530

    Created ViewAllJobs component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'; /* Import component */
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 
 const App = () => {
@@ -15,13 +16,7 @@ const App = () => {
       <Hero />
       <HomeCards />  
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-          >View All Jobs</a>
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
