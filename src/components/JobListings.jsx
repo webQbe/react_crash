19d1a74c9e988843b46ea75ea3904ea1d9537e82
webQbe <webcubesolutionslk@gmail.com>
2025-01-13 13:30:32 +0530

    Created recentJobs array to display first 3 jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,7 +3,9 @@ import jobs from '../jobs.json'; /* Import data from json file */
 import JobAd from '../components/JobAd'; /* Import JobAd component */
 
 const JobListings = () => {
-    console.log(jobs); // Log jobs data
+  // Only get first 3 jobs from jobs array
+  const recentJobs = jobs.slice(0, 3); 
+
   return (
     <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
@@ -11,7 +13,7 @@ const JobListings = () => {
                 Browse Jobs
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                { jobs.map((job) => (
+                { recentJobs.map((job) => (
                     <JobAd key={ job.id } job={ job } />    
                 ))}
             </div>
