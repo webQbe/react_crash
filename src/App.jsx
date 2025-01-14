3c3e83a2bbbf48b5f2c1152355895c87f4195488
webQbe <webcubesolutionslk@gmail.com>
2025-01-14 15:07:10 +0530

    Created HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,16 @@ import {
           RouterProvider 
         } from 'react-router-dom';
 
-import Navbar from './components/Navbar'; /* Import JSX component */
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
+       
 
 /* Create router variable */
 const router = createBrowserRouter(
   /* createBrowserRouter: Accepts the routes generated below and creates a router instance that manages the application's navigation. */
 
-  createRoutesFromElements(<Route path='/about' element={<h1>My App</h1>} />)
+  createRoutesFromElements(<Route index element={<HomePage />} />)
   /* Route: Represents a single route in the application 
-     index / path='/': Indicates this is the default route when the app loads
+     index / path='/about': Indicates this is the default route when the app loads
      element: Specifies the component or element to render when this route matches. 
      createRoutesFromElements(): Converts JSX route elements into a data structure that createBrowserRouter() understands. 
     */
