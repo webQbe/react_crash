ba6ca1fde8b816ff1d7145b327e60c3ff85be4ad
webQbe <webcubesolutionslk@gmail.com>
2025-01-14 14:50:53 +0530

    Changed default route

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ import ViewAllJobs from './components/ViewAllJobs';
 const router = createBrowserRouter(
   /* createBrowserRouter: Accepts the routes generated below and creates a router instance that manages the application's navigation. */
 
-  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+  createRoutesFromElements(<Route path='/about' element={<h1>My App</h1>} />)
   /* Route: Represents a single route in the application 
-     index: Indicates this is the default route when the app loads
+     index / path='/': Indicates this is the default route when the app loads
      element: Specifies the component or element to render when this route matches. 
      createRoutesFromElements(): Converts JSX route elements into a data structure that createBrowserRouter() understands. 
     */
