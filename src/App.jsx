2644423b6b1819cb3aabff7f9d2523b70cebde6d
webQbe <webcubesolutionslk@gmail.com>
2025-01-16 18:56:24 +0530

    Created JobsPage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
         } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
        
 
 /* Create router variable */
@@ -18,7 +19,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
 
-    <Route index element={<HomePage />} />   
+    <Route index element={<HomePage />} />  
+    <Route path='/jobs' element={<JobsPage />} />
 
   </Route>
 
