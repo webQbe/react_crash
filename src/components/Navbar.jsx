e060e7f244905e7de844118b3e778934c4187b5f
webQbe <webcubesolutionslk@gmail.com>
2025-01-16 19:49:18 +0530

    Added NavLink tags to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 /* rafce */
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png'
 import React from 'react'
 
@@ -10,17 +10,17 @@ const Navbar = () => {
       <div className="flex h-20 items-center justify-between">
         <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
           {/* <!-- Logo --> */}
-          <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+          <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
             <img className="h-10 w-auto" src={ logo } alt="React Jobs" />
             <span className="hidden md:block text-white text-2xl font-bold ml-2">
               React Jobs
             </span>
-          </Link>
+          </NavLink>
           <div className="md:ml-auto">
             <div className="flex space-x-2">
-              <Link to="/" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</Link>
-              <Link to="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</Link>
-              <Link to="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Job</Link>
+              <NavLink to="/" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</NavLink>
+              <NavLink to="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</NavLink>
+              <NavLink to="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Job</NavLink>
             </div>
           </div>
         </div>
