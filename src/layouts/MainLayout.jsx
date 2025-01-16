6759bd12b071a05aa0d79f133ff38de4b8b69a94
webQbe <webcubesolutionslk@gmail.com>
2025-01-16 18:39:51 +0530

    Added <Navbar /> component to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar'; /* Import Component */
  
 const MainLayout = () => {
   return (
     <>
+        <Navbar />
         <Outlet />
     </>
   );
@@ -13,8 +15,9 @@ export default MainLayout
 
 /* MainLayout Component:
     Acts as a wrapper or layout for the application.
-    Contains an <Outlet /> component.
-    
+    Includes <Navbar /> component for any page
+    Contains an <Outlet /> placeholder component.
+
     <Outlet />:
     A placeholder that renders child route components inside the MainLayout.
     For example, if the child route is the HomePage, the <Outlet /> in MainLayout will render the HomePage component.
