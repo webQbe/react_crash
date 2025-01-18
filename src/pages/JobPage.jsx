a8b33e93a9322d008a278d44834e8876b3b4394e
webQbe <webcubesolutionslk@gmail.com>
2025-01-18 18:23:37 +0530

    Added dynamic link to Edit Job button

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -86,7 +86,7 @@ const JobPage = () => {
  
                     <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                     <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                    <Link to="/add-job" className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit Job</Link>
+                    <Link to={`/jobs/edit/${job.id}`} className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit Job</Link>
 
                     <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Delete Job</button>
                     </div>
